abce0e0150e373a0fd9f2d179a9104b7ebd21608
Davy ROBERT <davyrobert90@gmail.com>
2022-12-12 11:14:51 +0100

    up

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.APP_PORT ?? 5008;
+
+app.use(express.json());
+
+const welcome = (req, res) => {
+  res.send('Welcome to hell');
+};
+
+app.get('/', welcome);
+
+const movieHandlers = require('./movieHandlers');
+const usersHandlers = require('./usersHandlers');
+const { validateMovie } = require('./validators.js');
+const { validateUser } = require('./validators.js');
+const { hashPassword } = require('./auth.js');
+
+app.get('/api/movies', movieHandlers.getMovies);
+app.get('/api/movies/:id', movieHandlers.getMovieById);
+app.get('/api/users', usersHandlers.getUsers);
+app.get('/api/users/:id', usersHandlers.getUsersById);
+
+app.post('/api/movies', validateMovie, movieHandlers.postMovie);
+app.post('/api/users', hashPassword, usersHandlers.postUser);
+
+app.put('/api/users/:id', validateUser, usersHandlers.updateUsers);
+app.put('/api/movies/:id', validateMovie, movieHandlers.updateMovie);
+
+app.delete('/api/movies/:id', movieHandlers.deleteMovie);
+app.delete('/api/users/:id', usersHandlers.deleteUser);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Something bad happened');
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
